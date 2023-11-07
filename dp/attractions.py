4f75458810243a3671d9539d1a6db3ab0b2ae040
Thien K. M. Bui <kevinbui904@gmail.com>
2023-11-06 18:05:38 -0600

    more work

--- dp/attractions.py
+++ dp/attractions.py
@@ -19,8 +19,8 @@ from sys import argv
 def main():
     arg_count = len(argv)
     print("Params passed", arg_count - 1, ":", argv[1:])
-    if(arg_count > 2):
-        print("Only one argument is allowed")
+    if(arg_count > 3):
+        print("Only two arguments are allowed")
         quit()
 
     #we first need to load the txt file into memory
@@ -34,38 +34,64 @@ def main():
       
     #algorithm
     
-    highest_costs = [0]*len(attractions)
+    highest_costs = [(0,0)]*len(attractions)
     #notice that the minimalis cost of visiting every attraction 
     #is the difference between the maximal cost of missing
     #every possible attraction and the total cost
-    highest_costs[0] = (attractions[0]["cost"], -1)
-    highest_costs[1] = (attractions[1]["cost"], -1)
+
+    #for keeping track of the sequence that results in the maximum sum
+    global_prev = None 
+    global_max = attractions[0]["cost"]    
+    highest_costs[0] = (attractions[0]["cost"], 0)
+    if attractions[1]["cost"] > attractions[0]["cost"]:
+        highest_costs[1] = (attractions[1]["cost"], 1)
+        global_max = attractions[1]["cost"]
+        global_prev = 1
+    else:
+        highest_costs[1] = (attractions[0]["cost"],0)
+        global_prev = 0
+    print(global_max)
     total_cost = attractions[0]["cost"] + attractions[1]["cost"]
     for i in range(2, len(attractions)):
-        (prevCost, prev) = highest_costs[i-1]
-        (prevPrevCost, prevPrev) = highest_costs[i-2]
-        if attractions[i]["cost"] + prevPrevCost > prevCost:
-            highest_costs[i] = (attractions[i]["cost"]+prevPrevCost, i-2)
-        else:
-            highest_costs[i] = (prevCost, i-1)
+        (prev_cost, prev) = highest_costs[i-1]
+        (prev_prev_cost, prev_prev) = highest_costs[i-2]
+        if attractions[i]["cost"] + prev_prev_cost > prev_cost:
+            highest_costs[i] = (attractions[i]["cost"] + prev_prev_cost, prev_prev)
+            if attractions[i]["cost"] + prev_prev_cost > global_max:
+                global_max = attractions[i]["cost"] + prev_prev_cost
+                global_prev = i
+                print("here", global_prev)
+                
+    #     (prev_cost, prev) = highest_costs[i-1]
+    #     (prev_prev_cost, prev_prev) = highest_costs[i-2]
+    #     if attractions[i]["cost"] + prev_prev_cost > prev_cost:
+    #         if attractions[i]["cost"]+prev_prev_cost > global_max:
+    #             global_prev = i
+    #         highest_costs[i] = (attractions[i]["cost"]+prev_prev_cost, i-2)
+    #     else:
+    #         print("HERE", prev, i, highest_costs[4])
+    #         highest_costs[i] = (prev_cost, global_prev)
         total_cost += attractions[i]["cost"]
+    print(global_prev)
+    print(highest_costs)
 
-    
+    quit()
     
     #This is the list of the most expensive, skippable attractions
     #We'll just need to trim it so that it is less than or equal to k
     
     # expensive_attractions
-    expensive_attractions = [attractions[-1]]
-    (prevCost, prev) = highest_costs[-1]
-    while prev > -1:
+    (cost, prev) = highest_costs[-1]
+    expensive_attractions = [attractions[prev]]
+    while prev > 1:
         expensive_attractions.append(attractions[prev])
-        (prevCost, prev) = highest_costs[prev]
-    print(attractions)
-    print(highest_costs)
+        (cost, prev) = highest_costs[prev]
+    expensive_attractions = sorted(expensive_attractions, key=lambda d: d["cost"], reverse=True)
+
+    print(expensive_attractions[:int(argv[2])])
     print(total_cost - highest_costs[-1][0])
-    print(expensive_attractions)
-    
+    print(highest_costs)    
+    print(highest_costs[-1])
 
 if __name__ == "__main__":
     main()
